779f20a8e9d373babd857ce896b6497fb6f5b249
Joonas Häkkinen <joonas.hakkinen@crm-service.fi>
2019-09-05 21:41:12 +0300

    puhelinluettelo step1

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <div>
+      {person.name}
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const numbers = () => persons.map(person =>
+    <Person
+      key={person.name}
+      person={person}
+    />
+  )
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {numbers()}
+      </div>
+    </div>
+  )
+
+}
+
+export default App
